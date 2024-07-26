17dfb1fc73189f0194e7581eafc31734fd387b5a
Neteree <neteree@gmail.com>
2024-07-26 19:51:19 +1200

    - The projects gui now starts off as disabled - The projects gui will now be opened and enabled when clicking on an npc - The button text of said gui will be set to the projects relevant to the npc specified skill/language - All skils/npcs have some relevant projects that will pop up in the gui now - A module has been created to contain the texts relevant to each skill specified by a key - Functions have been created and other things have been done to make the code more readable and dry

---
+++ main/texts_by_skill.lua
@@ -0,0 +1,39 @@
+local texts_by_skill = {
+	["c++"] = {
+		"Coming Soon",
+		"Coming Soon",
+		"Coming Soon",
+		"Coming Soon",
+		"Coming Soon",
+	},
+	["python"] = {
+		"Ticket System",
+		"Whitecliffe Code Practice",
+		"Coming Soon",
+		"Coming Soon",
+		"Coming Soon",
+	},
+	["lua"] = {
+		"Portfolio Site",
+		"Koen Api",
+		"Ko Game",
+		"First Mini Defold Project",
+		"Practice Code",
+	},
+	["c#"] = {
+		"Koen Crud",
+		"Practice Code",
+		"Coming Soon",
+		"Coming Soon",
+		"Coming Soon",
+	},
+	["javascript_typescript"] = {
+		"Truffle Pig Game",
+		"Mover Bot Game",
+		"Rough Portfolio Prototype",
+		"Practice Code",
+		"Coming Soon",
+	},
+}
+
+return texts_by_skill
